b4fcfa9d5afa517482e52cb2e1f2eb83c43e3efa
apoorvk <apoorvknd@yahoo.com>
2015-02-28 20:20:13 -0800

    Added URL link

--- app/src/main/java/com/teslastem/aquifereducation/AboutUs.java
+++ app/src/main/java/com/teslastem/aquifereducation/AboutUs.java
@@ -1,27 +1,26 @@
 package com.teslastem.aquifereducation;
-
 import android.app.Activity;
 import android.graphics.Typeface;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-
 public class AboutUs extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
         TextView description = (TextView) findViewById(R.id.textView2);
         TextView title = (TextView) findViewById(R.id.textView7);
+        TextView URL = (TextView) findViewById(R.id.textView3);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         description.setTypeface(font);
         title.setTypeface(font);
-
+        URL.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW,
+                        android.net.Uri.parse("http://aquifereducation.wix.com/teslastem")));
+            }
+        });
     }
-
 }
